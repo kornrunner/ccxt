54c0eeee5f3df1107677445003ae7cab3c6fe353
Boris Momčilović <boris@firstbeatmedia.com>
2017-11-21 12:33:20 +0100

    PHP only

---
+++ examples/order-book-level-depth-extra-param.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+date_default_timezone_set ('UTC');
+
+// instantiate the exchange by id
+$exchange = '\\ccxt\\kraken';
+$exchange = new $exchange ();
+var_dump ($exchange->fetch_order_book ('BTC/USD', array (
+    'count' => 10, // up to ten order on each side for example
+)));
