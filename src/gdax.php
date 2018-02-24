c806da07cb4cf9313b1564c450f046be760490c3
Boris Momčilović <boris@firstbeatmedia.com>
2018-02-24 02:07:14 +0100

    Sync

--- src/gdax.php
+++ src/gdax.php
@@ -79,6 +79,7 @@ class gdax extends Exchange {
                     'post' => array (
                         'deposits/coinbase-account',
                         'deposits/payment-method',
+                        'coinbase-accounts/{id}/addresses',
                         'funding/repay',
                         'orders',
                         'position/close',
