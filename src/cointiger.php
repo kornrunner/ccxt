2db30612e125b117358f88c071d0e281bf1a1bb1
Boris Momčilović <boris@firstbeatmedia.com>
2018-05-31 14:01:12 +0200

    Sync

--- src/cointiger.php
+++ src/cointiger.php
@@ -67,6 +67,8 @@ class cointiger extends huobipro {
                 '1' => '\\ccxt\\InsufficientFunds',
                 '2' => '\\ccxt\\ExchangeError',
                 '5' => '\\ccxt\\InvalidOrder',
+                '6' => '\\ccxt\\InvalidOrder',
+                '8' => '\\ccxt\\OrderNotFound',
                 '16' => '\\ccxt\\AuthenticationError', // funding password not set
                 '100001' => '\\ccxt\\ExchangeError',
                 '100002' => '\\ccxt\\ExchangeNotAvailable',
@@ -483,7 +485,7 @@ class cointiger extends huobipro {
         $timestamp = $this->milliseconds ();
         return array (
             'info' => $response,
-            'id' => (string) $response['order_id'],
+            'id' => (string) $response['data']['order_id'],
             'timestamp' => $timestamp,
             'datetime' => $this->iso8601 ($timestamp),
             'lastTradeTimestamp' => null,
@@ -506,10 +508,15 @@ class cointiger extends huobipro {
         if ($symbol === null)
             throw new ExchangeError ($this->id . ' cancelOrder requires a $symbol argument');
         $market = $this->market ($symbol);
-        return $this->privateDeleteOrder (array_merge (array (
+        $response = $this->privateDeleteOrder (array_merge (array (
             'symbol' => $market['id'],
             'order_id' => $id,
         ), $params));
+        return array (
+            'id' => $id,
+            'symbol' => $symbol,
+            'info' => $response,
+        );
     }
 
     public function sign ($path, $api = 'public', $method = 'GET', $params = array (), $headers = null, $body = null) {
