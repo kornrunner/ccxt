de2f3b229f8cc1ba884a9740b9e8fbd819e4d586
Boris Momčilović <boris@firstbeatmedia.com>
2018-06-23 23:06:27 +0200

    Sync

--- src/gemini.php
+++ src/gemini.php
@@ -65,6 +65,7 @@ class gemini extends Exchange {
                         'deposit/{currency}/newAddress',
                         'withdraw/{currency}',
                         'heartbeat',
+                        'transfers',
                     ),
                 ),
             ),
