ee3d2d43c4ed0a729d547167b6f685cba01e4035
Boris Momčilović <boris@firstbeatmedia.com>
2018-11-13 14:09:13 +0100

    Sync

--- src/bleutrade.php
+++ src/bleutrade.php
@@ -93,6 +93,7 @@ class bleutrade extends bittrex {
             ),
             'options' => array (
                 'parseOrderStatus' => true,
+                'disableNonce' => false,
             ),
         ));
     }
