024a982f48de915c7e2b138a81e8a90486186378
Boris Momčilović <boris@firstbeatmedia.com>
2018-11-15 16:41:46 +0100

    Sync

--- src/binance.php
+++ src/binance.php
@@ -93,7 +93,6 @@ class binance extends Exchange {
                 ),
                 'public' => array (
                     'get' => array (
-                        'exchangeInfo',
                         'ping',
                         'time',
                         'depth',
