4167a4f6c89775e6749dc6f89131b5d8657261bc
Boris Momčilović <boris@firstbeatmedia.com>
2019-01-16 12:35:24 +0100

    Sync

--- src/kraken.php
+++ src/kraken.php
@@ -933,8 +933,14 @@ class kraken extends Exchange {
     }
 
     public function parse_transaction_status ($status) {
+        // IFEX transaction states
         $statuses = array (
+            'Initial' => 'pending',
+            'Pending' => 'pending',
             'Success' => 'ok',
+            'Settled' => 'ok',
+            'Failure' => 'failed',
+            'Partial' => 'ok',
         );
         return $this->safe_string($statuses, $status, $status);
     }
@@ -986,6 +992,11 @@ class kraken extends Exchange {
         $status = $this->parse_transaction_status ($this->safe_string($transaction, 'status'));
         $type = $this->safe_string($transaction, 'type'); // injected from the outside
         $feeCost = $this->safe_float($transaction, 'fee');
+        if ($feeCost === null) {
+            if ($type === 'deposit') {
+                $feeCost = 0;
+            }
+        }
         return array (
             'info' => $transaction,
             'id' => $id,
