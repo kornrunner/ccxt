7212db07a7e528f97ce34cdadb1ae601d3a11cde
Boris Momčilović <boris@firstbeatmedia.com>
2019-07-11 11:15:33 +0200

    Sync

--- src/Exchange.php
+++ src/Exchange.php
@@ -34,7 +34,7 @@ use kornrunner\Eth;
 use kornrunner\Secp256k1;
 use kornrunner\Solidity;
 
-$version = '1.18.919';
+$version = '1.18.922';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -51,7 +51,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.18.919';
+    const VERSION = '1.18.922';
 
     public static $eth_units = array (
         'wei'        => '1',
@@ -101,7 +101,6 @@ class Exchange {
         'bithumb',
         'bitkk',
         'bitlish',
-        'bitmarket',
         'bitmex',
         'bitso',
         'bitstamp',
@@ -137,7 +136,6 @@ class Exchange {
         'coingi',
         'coinmarketcap',
         'coinmate',
-        'coinnest',
         'coinone',
         'coinspot',
         'cointiger',
