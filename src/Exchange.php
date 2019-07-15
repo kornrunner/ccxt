9b22e3fe10f23c5f6244e52ba034b8c064a079c2
Boris Momčilović <boris@firstbeatmedia.com>
2019-07-15 16:14:39 +0200

    Sync

--- src/Exchange.php
+++ src/Exchange.php
@@ -34,7 +34,7 @@ use kornrunner\Eth;
 use kornrunner\Secp256k1;
 use kornrunner\Solidity;
 
-$version = '1.18.923';
+$version = '1.18.941';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -51,7 +51,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.18.923';
+    const VERSION = '1.18.941';
 
     public static $eth_units = array (
         'wei'        => '1',
@@ -113,7 +113,6 @@ class Exchange {
         'btcalpha',
         'btcbox',
         'btcchina',
-        'btcexchange',
         'btcmarkets',
         'btctradeim',
         'btctradeua',
@@ -121,7 +120,6 @@ class Exchange {
         'buda',
         'bxinth',
         'cex',
-        'chbtc',
         'chilebit',
         'cobinhood',
         'coinbase',
@@ -157,7 +155,6 @@ class Exchange {
         'gateio',
         'gdax',
         'gemini',
-        'hadax',
         'hitbtc',
         'hitbtc2',
         'huobipro',
--- src/hitbtc.php
+++ src/hitbtc.php
@@ -482,7 +482,6 @@ class hitbtc extends Exchange {
                 ),
             ),
             'commonCurrencies' => array (
-                'BCH' => 'Bitcoin Cash',
                 'BET' => 'DAO.Casino',
                 'CAT' => 'BitClave',
                 'DRK' => 'DASH',
