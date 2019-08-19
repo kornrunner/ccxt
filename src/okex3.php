4e009d54ec11504f164ee9efa559ffd5fccd5da0
Boris Momčilović <boris@firstbeatmedia.com>
2019-08-19 11:41:02 +0200

    Sync

--- src/okex3.php
+++ src/okex3.php
@@ -448,6 +448,7 @@ class okex3 extends Exchange {
                 'HSR' => 'HC',
                 'MAG' => 'Maggie',
                 'YOYO' => 'YOYOW',
+                'WIN' => 'WinToken', // https://github.com/ccxt/ccxt/issues/5701
             ),
         ));
     }
