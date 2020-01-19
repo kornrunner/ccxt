18f683a8a97733c1064d8eb500482e65a9adc76f
Boris Momčilović <boris@firstbeatmedia.com>
2020-01-19 21:36:26 +0100

    Sync

--- src/bigone.php
+++ src/bigone.php
@@ -22,6 +22,7 @@ class bigone extends Exchange {
                 'fetchDeposits' => true,
                 'fetchMyTrades' => true,
                 'fetchOHLCV' => true,
+                'fetchOrder' => true,
                 'fetchOrders' => true,
                 'fetchOpenOrders' => true,
                 'fetchClosedOrders' => true,
