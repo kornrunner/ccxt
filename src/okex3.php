a634cfdb4c20037ef0c1e8381976cbfe1637ceec
Boris Momčilović <boris@firstbeatmedia.com>
2020-03-12 12:19:41 +0100

    Sync

--- src/okex3.php
+++ src/okex3.php
@@ -94,6 +94,7 @@ class okex3 extends Exchange {
                         'orders/{client_oid}',
                         'fills',
                         'algo',
+                        'trade_fee',
                         // public
                         'instruments',
                         'instruments/{instrument_id}/book',
@@ -151,6 +152,7 @@ class okex3 extends Exchange {
                         'orders/{instrument_id}/{order_id}',
                         'orders/{instrument_id}/{client_oid}',
                         'fills',
+                        'trade_fee',
                         // public
                         'instruments',
                         'instruments/{instrument_id}/book',
@@ -195,6 +197,7 @@ class okex3 extends Exchange {
                         'orders/{instrument_id}/{order_id}',
                         'orders/{instrument_id}/{client_oid}',
                         'fills',
+                        'trade_fee',
                         // public
                         'instruments',
                         'instruments/{instrument_id}/depth',
