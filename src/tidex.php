39dad5646a1975a1385a3392198f402777a99207
Boris Momčilović <boris@firstbeatmedia.com>
2020-06-02 15:49:12 +0200

    Sync

--- src/tidex.php
+++ src/tidex.php
@@ -601,6 +601,7 @@ class tidex extends Exchange {
         return array(
             'info' => $order,
             'id' => $id,
+            'clientOrderId' => null,
             'symbol' => $symbol,
             'timestamp' => $timestamp,
             'datetime' => $this->iso8601($timestamp),
@@ -614,7 +615,6 @@ class tidex extends Exchange {
             'filled' => $filled,
             'status' => $status,
             'fee' => $fee,
-            'clientOrderId' => null,
             'average' => null,
             'trades' => null,
         );
