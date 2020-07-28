d3b1a134455c3988051de3534cdbacd0e9308a93
Boris Momčilović <boris@firstbeatmedia.com>
2020-07-28 21:20:59 +0200

    Sync

--- src/okex.php
+++ src/okex.php
@@ -2482,7 +2482,7 @@ class okex extends Exchange {
         //
         $statuses = array(
             '-3' => 'pending',
-            '-2' => 'pending',
+            '-2' => 'canceled',
             '-1' => 'failed',
             '0' => 'pending',
             '1' => 'pending',
