fea8e8381faa5d7a9f83002f93ea7bdffe241d77
Boris Momčilović <boris@firstbeatmedia.com>
2021-03-03 16:54:30 +0100

    Sync

--- src/bitfinex.php
+++ src/bitfinex.php
@@ -16,7 +16,6 @@ class bitfinex extends Exchange {
             'countries' => array( 'VG' ),
             'version' => 'v1',
             'rateLimit' => 1500,
-            'certified' => true,
             'pro' => true,
             // new metainfo interface
             'has' => array(
